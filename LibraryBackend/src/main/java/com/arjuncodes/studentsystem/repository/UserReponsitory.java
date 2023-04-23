89f38ed5b9ea53d46524c4c217d5a900483473b4
Vuong Nguyen <trungvuong2169@gmail.com>
2023-04-23 22:55:04 +0700

    lancuoi

--- LibraryBackend/src/main/java/com/arjuncodes/studentsystem/repository/UserReponsitory.java
+++
@@ -1,9 +0,0 @@
-package com.arjuncodes.studentsystem.repository;
-
-import com.arjuncodes.studentsystem.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserReponsitory extends JpaRepository<User,Long> {
-}
